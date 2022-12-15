244a72eee29e162a27dc9c03a664b6ff3a3af1cc
Nafiz Mohammad Hannan <111026607+nafizmh@users.noreply.github.com>
2022-12-15 22:15:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,176 @@
+// ----------------0------------------------------
+var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+
+copyTextareaBtn.addEventListener('click', function (event) {
+    var copyTextarea = document.querySelector('.js-copytextarea');
+    copyTextarea.focus();
+    copyTextarea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------1------------------------------
+var copyTextareaBtn1 = document.querySelector('.js-textareacopybtn1');
+
+copyTextareaBtn1.addEventListener('click', function (event) {
+    var copyTextarea1 = document.querySelector('.js-copytextarea1');
+    copyTextarea1.focus();
+    copyTextarea1.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------2------------------------------
+var copyTextareaBtn2 = document.querySelector('.js-textareacopybtn2');
+
+copyTextareaBtn2.addEventListener('click', function (event) {
+    var copyTextarea2 = document.querySelector('.js-copytextarea2');
+    copyTextarea2.focus();
+    copyTextarea2.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------3------------------------------
+var copyTextareaBtn3 = document.querySelector('.js-textareacopybtn3');
+
+copyTextareaBtn3.addEventListener('click', function (event) {
+    var copyTextarea3 = document.querySelector('.js-copytextarea3');
+    copyTextarea3.focus();
+    copyTextarea3.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------4------------------------------
+var copyTextareaBtn4 = document.querySelector('.js-textareacopybtn4');
+
+copyTextareaBtn4.addEventListener('click', function (event) {
+    var copyTextarea4 = document.querySelector('.js-copytextarea4');
+    copyTextarea4.focus();
+    copyTextarea4.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------5------------------------------
+var copyTextareaBtn5 = document.querySelector('.js-textareacopybtn5');
+
+copyTextareaBtn5.addEventListener('click', function (event) {
+    var copyTextarea5 = document.querySelector('.js-copytextarea5');
+    copyTextarea5.focus();
+    copyTextarea5.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------6------------------------------
+var copyTextareaBtn6 = document.querySelector('.js-textareacopybtn6');
+
+copyTextareaBtn6.addEventListener('click', function (event) {
+    var copyTextarea6 = document.querySelector('.js-copytextarea6');
+    copyTextarea6.focus();
+    copyTextarea6.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------7------------------------------
+var copyTextareaBtn7 = document.querySelector('.js-textareacopybtn7');
+
+copyTextareaBtn7.addEventListener('click', function (event) {
+    var copyTextarea7 = document.querySelector('.js-copytextarea7');
+    copyTextarea7.focus();
+    copyTextarea7.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------8------------------------------
+var copyTextareaBtn8 = document.querySelector('.js-textareacopybtn8');
+
+copyTextareaBtn8.addEventListener('click', function (event) {
+    var copyTextarea8 = document.querySelector('.js-copytextarea8');
+    copyTextarea8.focus();
+    copyTextarea8.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------9------------------------------
+var copyTextareaBtn9 = document.querySelector('.js-textareacopybtn1');
+
+copyTextareaBtn9.addEventListener('click', function (event) {
+    var copyTextarea9 = document.querySelector('.js-copytextarea9');
+    copyTextarea9.focus();
+    copyTextarea9.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
+// ----------------10------------------------------
+var copyTextareaBtn10 = document.querySelector('.js-textareacopybtn10');
+
+copyTextareaBtn10.addEventListener('click', function (event) {
+    var copyTextarea10 = document.querySelector('.js-copytextarea10');
+    copyTextarea10.focus();
+    copyTextarea10.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Copying text command was ' + msg);
+    } catch (err) {
+        alert('Oops, unable to copy');
+    }
+});
